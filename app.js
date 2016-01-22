9b8f476681a6d965a461db2eaf7c128f24bdb667
namlgiang <giangnam@nam.local>
2016-01-22 23:02:15 +0700

    init
    init

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var messages = [];
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/history', function(req, res) {
+  res.send(messages);
+});
+
+io.on('connection', function(socket) {
+  console.log('an user connected');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+    console.log(msg);
+    messages.push(msg);
+  });
+});
+
+http.listen(process.env.PORT || 8080);
