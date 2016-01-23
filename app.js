f232c8f2289aef8485742d25b055d41b53f16b51
namlgiang <giangnam@nam.local>
2016-01-23 19:42:21 +0700

    change port to 8080

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(process.env.PORT || 80, function() {
+http.listen(process.env.PORT || 8080, function() {
   console.log("Listening...");
 });
