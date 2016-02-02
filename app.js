0d31ae56853fe9d2782181476de7a1bcda6ff613
namlgiang <giangnam@nam.local>
2016-02-02 11:17:19 +0700

    add find match

--- app.js
+++ app.js
@@ -3,23 +3,190 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var messages = [];
+/* Rooms:
+[
+{id:
+messages:
+users: []}
+] */
+var rooms = [];
+
+/* Connecting users:
+[
+{username:
+key:
+socket:
+room:}
+] */
+var users = [];
+
+//Waiting user
+var waitingUser;
+
+
 
 app.use(express.static(__dirname + '/client'));
 
-app.get('/history', function(req, res) {
-  res.send(messages);
-});
+function disconnectUser(user) {
+  if(user.room != undefined) {
+    io.to(user.room.id).emit('disconnect', user.username);
+  }
+}
 
 io.on('connection', function(socket) {
-  console.log('an user connected');
-  socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
-    console.log(msg);
-    messages.push(msg);
+  console.log('an user connected ' + socket.id);
+
+  // Leave
+  socket.on('leave', function() {
+    for(var i=0; i<users.length; i++)
+      if(users[i].socket.id == socket.id && users[i].room != undefined) {
+        io.to(users[i].room.id).emit('left', users[i].username);
+        socket.leave(users[i].room);
+        users[i].room.activeUser--;
+        if(users[i].room.activeUser == 0) {
+          for(var j=0; j<rooms.length; j++)
+            if(rooms[j].id == users[i].room.id) {
+              rooms.splice(j,1);
+              break;
+            }
+        }
+        users[i].room = undefined;
+        socket.emit('leave complete');
+      }
   });
+
+  // On disconnect:
+  // Stop finding match
+  // Leave room
+  socket.on('disconnect', function() {
+    console.log('- an user disconnected ' + socket.id);
+    if(waitingUser != undefined && waitingUser.socket.id == socket.id)
+      waitingUser = undefined;
+    for(var i=0; i<users.length; i++)
+      if(users[i].socket.id == socket.id)
+        disconnectUser(users[i]);
+  });
+
+  // Authenticate with client
+  socket.on('authenticate', function(user) {
+    console.log('authenticate ' + user.username);
+    for(var i=0; i<users.length; i++)
+      if(users[i].username == user.username && users[i].key == user.key) {
+        users[i].socket = socket;
+        var ul = [];
+        if(users[i].room != undefined)
+          for(var j=0; j<users[i].room.users.length; j++)
+            ul.push(users[i].room.users[j].username);
+        socket.emit('authenticate complete', {
+          valid: true,
+          hasRoom: users[i].room == undefined ? false : users[i].room.id,
+          userList: users[i].room == undefined ? false : ul
+        });
+        if(users[i].room != undefined) {
+          console.log("Old room id: " + users[i].room.id);
+          socket.join(users[i].room.id);
+          socket.emit('chat history', users[i].room.messages);
+          io.to(users[i].room.id).emit('reconnect', user.username);
+        }
+        return;
+      }
+    socket.emit('authenticate complete', {
+      validate: false
+    });
+  });
+
+  // Register new user
+  socket.on('register', function(user) {
+    for(var i=0; i<users.length; i++)
+      if(users[i].username == user.username) {
+        socket.emit('register complete', {
+          err: "username already exists!"
+        });
+        return;
+      }
+
+    var newUser = {
+      "username": user.username,
+      "key": randomString(32),
+      "socket": socket,
+      "room": undefined
+    };
+    users.push(newUser);
+    socket.emit('register complete', {
+      "username": newUser.username,
+      "key": newUser.key
+    });
+  });
+
+  // Log out
+  socket.on('logout', function() {
+    for(var i=0; i<users.length; i++)
+      if(users[i].socket.id == socket.id) {
+        disconnectUser(users[i]);
+        users.splice(i,1);
+        socket.emit('logout complete', true);
+        return;
+      }
+    socket.emit('logout complete', false);
+  });
+
+  // Find match
+  socket.on('find match', function() {
+    for(var i=0; i<users.length; i++)
+      if(users[i].socket.id == socket.id) {
+        if(waitingUser == undefined)
+          waitingUser = users[i];
+        else {
+          // Notify to both user and move to new room
+          var newRoom = waitingUser.username + '_' + users[i].username;
+
+          // Add new room
+          var roomObj = {
+            id: newRoom,
+            users: [waitingUser, users[i]],
+            messages: [],
+            activeUser: 2
+          };
+          rooms.push(roomObj);
+          users[i].room = roomObj;
+          waitingUser.room = roomObj;
+
+          waitingUser.socket.join(newRoom);
+          users[i].socket.join(newRoom);
+
+          var data = {
+            userList: [waitingUser.username, users[i].username],
+            room: newRoom
+          };
+
+          waitingUser.socket.emit('found match', data);
+          users[i].socket.emit('found match', data);
+          waitingUser = undefined;
+        }
+      }
+  });
+  socket.on('stop match', function() {
+    if(waitingUser != undefined && waitingUser.socket.id == socket.id)
+      waitingUser = undefined;
+  });
+
+  // Send message
+  socket.on('chat', function(data) {
+    io.to(data.room).emit('chat', data);
+    for(var i=0; i<rooms.length; i++)
+      if(rooms[i].id == data.room)
+        rooms[i].messages.push(data);
+  });
+
 });
 
 http.listen(process.env.PORT || 8080, function() {
   console.log("Listening...");
 });
+
+function randomString(length) {
+    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
--- client/client.js
+++ client/client.js
@@ -1,16 +1,198 @@
 var socket = io();
+var user, room;
 
-$.get('/history', function(data) {
-  console.log(data);
+/*
+GLOBAL
+*/
+
+$(document).ready(function() {
+
+  if(!authenticate()) {
+    $(".login").addClass("active");
+  }
+
+  $(".username").focus();
+  $(".username").keypress(function(e) {
+    if(e.keyCode == 13)
+      login();
+  });
+
+  $(".submit").click(function() {
+    login();
+  });
+
+  $(".log-out").click(function() {
+    socket.emit('logout');
+  });
+
+  $(".find-match").click(function() {
+    if(!$(this).is(".active")) {
+      $(this).text("Finding match...").addClass("active");
+      socket.emit("find match");
+    }
+    else {
+      $(this).text("Find match").removeClass("active");
+      socket.emit("stop match");
+    }
+  });
+
+  $(".message").keyup(function(e) {
+    if(e.keyCode == 13)
+      sendMessage();
+  });
+  $(".send").click(function() {
+    sendMessage();
+  });
+
+  $(".leave").click(function() {
+    socket.emit("leave");
+  });
+
+});
+
+function sendMessage() {
+  if($(".message").val().trim() != "") {
+    socket.emit("chat", {
+      'room': room,
+      'name': user.username,
+      'msg': $(".message").val()
+    });
+    $(".message").val("");
+  }
+}
+
+/*
+RECEIVE
+*/
+
+socket.on('leave complete', function() {
+  room = undefined;
+  $(".chat").removeClass('active');
+  $(".main").addClass('active');
+  $(".find-match").text("Find match").removeClass("active");
+});
+
+socket.on('left', function(user) {
+  var li = $("<li>").addClass('disconnected').html(user + " left the room");
+  $(".messages").append(li);
+});
+
+socket.on('disconnect', function(user) {
+  var li = $("<li>").addClass('disconnected').html(user + " has just been disconnected");
+  li.css('opacity', 1).animate({opacity: 1}, 3000).animate({opacity: 0}, 500, function() {
+    $(this).remove();
+  });
+  $(".messages").append(li);
+});
+
+socket.on('reconnect', function(user) {
+  var li = $("<li>").addClass('reconnected').html(user + " has just been reconnected");
+  li.css('opacity', 1).animate({opacity: 1}, 3000).animate({opacity: 0}, 500, function() {
+    $(this).remove();
+  });
+  $(".messages").append(li);
+});
+
+// Chat history
+socket.on('chat history', function(data) {
   for(var i=0; i<data.length; i++)
-    $('#messages').append($('<li>').text(data[i]));
+    $(".messages").append("<li>" + data[i].name + ": " + data[i].msg + "</li>");
 });
 
-$('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
-  return false;
+// Receive message
+socket.on('chat', function(data) {
+  console.log(data);
+  $(".messages").append("<li>" + data.name + ": " + data.msg + "</li>");
 });
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+
+// On match found
+socket.on('found match', function(data) {
+  $(".main").removeClass("active");
+  $(".chat").addClass("active");
+  $(".messages").empty();
+  $(".chat .title").html(data.userList[0] + " & " + data.userList[1]);
+  room = data.room;
 });
+
+// On authenticate response
+socket.on('authenticate complete', function(data) {
+  console.log('authenticate: ');
+  console.log(data);
+  if(data.valid) {
+    $(".welcome .name").html(user.username);
+
+    if(data.hasRoom) {
+      room = data.hasRoom;
+      $(".chat .title").html(data.userList[0] + " & " + data.userList[1]);
+      $(".chat").addClass("active");
+      $(".main").removeClass("active");
+      $(".login").removeClass("active");
+    }
+    else {
+      $(".main").addClass("active");
+      $(".login").removeClass("active");
+      $(".welcome .name").html(user.username);
+    }
+
+  }
+  else {
+    $(".login").addClass("active");
+    $(".main").removeClass("active");
+  }
+});
+
+// On register response
+socket.on('register complete', function(data) {
+  if(data.err != undefined) {
+    $(".msg").html(data.msg);
+    console.log(data.msg);
+    return;
+  }
+  $(".msg").html("");
+  user = data;
+  $.cookie("user", JSON.stringify(user));
+  $(".welcome .name").html(user.username);
+  $(".main").addClass("active");
+  $(".login").removeClass("active");
+});
+
+// On log out response
+socket.on('logout complete', function(data) {
+  if(data) {
+    $(".login").addClass("active");
+    $(".main").removeClass("active");
+  }
+});
+
+/*
+SEND
+*/
+
+// Send authenticate data
+function authenticate() {
+  var cookie = $.cookie('user');
+  if(cookie == undefined)
+    return false;
+  user = $.parseJSON($.cookie('user'));
+  console.log("user from cookie: ");
+  console.log(user);
+  if(user == undefined)
+    return false;
+  socket.emit('authenticate', user);
+  return true;
+}
+
+// Send login request
+function login() {
+  $(".find-match").removeClass('active');
+
+  var u = $(".username").val();
+  if(u=="") {
+    $(".login").animate({left: "49%"}, 50).animate({left: "51%"}, 50). animate({left: "50%"}, 100);
+    $(".username").focus();
+    console.log("username cannot be empty");
+    return;
+  }
+  console.log("registering username");
+  socket.emit('register', {username: u});
+}
